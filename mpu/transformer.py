740e08b3203c16238eef98df6c396cefcd9265e0
duzx <duzx16@mails.tsinghua.edu.cn>
2020-11-02 15:41:10 +0800

    Remove torch.jit

--- mpu/transformer.py
+++ mpu/transformer.py
@@ -156,7 +156,7 @@ class GPT2ParallelSelfAttention(torch.nn.Module):
         return output
 
 
-@torch.jit.script
+# @torch.jit.script
 def gelu_impl(x):
      """OpenAI's gelu implementation."""
      return 0.5 * x * (1.0 + torch.tanh(0.7978845608028654 * x *
