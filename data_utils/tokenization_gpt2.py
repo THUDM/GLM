57ab9a129d153f36c187cee3ce33fa94faa621ea
duzx <duzx16@mails.tsinghua.edu.cn>
2020-11-02 15:40:57 +0800

    Change data and vocab path

--- data_utils/tokenization_gpt2.py
+++ data_utils/tokenization_gpt2.py
@@ -36,10 +36,10 @@ from .file_utils import cached_path
 logger = logging.getLogger(__name__)
 
 PRETRAINED_VOCAB_ARCHIVE_MAP = {
-    'gpt2': "/mnt/.pytorch_pretrained_bert/gpt2-vocab.json",
+    'gpt2': ".pytorch_pretrained_bert/gpt2-vocab.json",
 }
 PRETRAINED_MERGES_ARCHIVE_MAP = {
-    'gpt2': "/mnt/.pytorch_pretrained_bert/gpt2-merges.txt",
+    'gpt2': ".pytorch_pretrained_bert/gpt2-merges.txt",
 }
 PRETRAINED_VOCAB_POSITIONAL_EMBEDDINGS_SIZE_MAP = {
     'gpt2': 1024,
