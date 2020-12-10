6709eb3831d93a4479a6b52ada81db0afe4cf711
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2020-12-10 09:22:42 +0800

    Add scripts for BlockLM

---
+++ scripts/ds_pretrain_block.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+# Change for multinode config
+
+NUM_WORKERS=8
+NUM_GPUS_PER_WORKER=8
+MP_SIZE=1
+
+script_path=$(realpath $0)
+script_dir=$(dirname $script_path)
+
+OPTIONS_NCCL="NCCL_DEBUG=info NCCL_IB_DISABLE=0 NCCL_SOCKET_IFNAME=bond0 NCCL_IB_GID_INDEX=3 NCCL_NET_GDR_LEVEL=0"
+HOST_FILE_PATH="/root/code/config/pre_hostfile"
+#OPTIONS_NCCL=""
+#HOST_FILE_PATH="/workspace/hostfile"
+
+
+config_json="$script_dir/ds_config.json"
+gpt_options=" \
+       --block-lm \
+       --experiment-name block-lm-base \
+       --model-parallel-size ${MP_SIZE} \
+       --num-layers 12 \
+       --hidden-size 768 \
+       --num-attention-heads 12 \
+       --seq-length 512 \
+       --max-position-embeddings 512 \
+       --save /root/data/checkpoints \
+       --train-iters 100000 \
+       --resume-dataloader \
+       --train-data bert \
+       --lazy-loader \
+       --tokenizer-type BertWordPieceTokenizer \
+       --tokenizer-model-type bert-base-uncased \
+       --split 949,50,1 \
+       --distributed-backend nccl \
+       --lr-decay-style cosine \
+       --lr-decay-iters 80000 \
+       --lr-decay-ratio 0.05 \
+       --warmup .01 \
+       --checkpoint-activations \
+       --deepspeed-activation-checkpointing \
+       --fp16 \
+"
+gpt_options="${gpt_options}
+               --deepspeed \
+               --deepspeed_config ${config_json} \
+"
+
+
+run_cmd="${OPTIONS_NCCL} deepspeed --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER} --hostfile ${HOST_FILE_PATH} pretrain_gpt2.py $@ ${gpt_options}"
+echo ${run_cmd}
+eval ${run_cmd}
+
+set +x
