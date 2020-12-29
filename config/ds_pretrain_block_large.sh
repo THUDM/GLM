95ec2f5a5a8ac00fafbc1eea01316fb746a39504
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2020-12-29 11:17:41 +0800

    Update BlockLM large

--- config/ds_pretrain_block_large.sh
+++ config/ds_pretrain_block_large.sh
@@ -6,6 +6,7 @@ script_dir=$(dirname $script_path)
 config_json="$script_dir/ds_config.json"
 gpt_options=" \
        --block-lm \
+      --bert-prob 0.5 \
        --experiment-name block-lm-large \
        --model-parallel-size ${MP_SIZE} \
        --num-layers 24 \
