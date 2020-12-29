7932f1ea471beda2015ab2afba886d22c8e970d8
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2020-12-29 22:35:08 +0800

    Update scripts

--- scripts/finetune_record_few.sh
+++ scripts/finetune_record_few.sh
@@ -1,6 +1,5 @@
 GPUS_PER_NODE=4
 MASTER_ADDR=localhost
-MASTER_PORT=6000
 NNODES=1
 NODE_RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
@@ -9,42 +8,43 @@ DATESTR=$(date +"%m-%d-%H-%M")
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
 DATA_PATH="/root/data/fewglue/ReCoRD"
+CHECKPOINT_PATH="/root/data/checkpoints"
+EXPERIMENT_NAME=blank-base-few
 PRETRAINED_CHECKPOINT=/root/data/checkpoints/block-lm-blank-cls12-18-12-50
-CHECKPOINT_PATH=/root/data/checkpoints
-EXPERIMENT_NAME=blank-base-record-few
+
 MODEL_ARGS="--block-lm \
+            --cloze-eval \
             --num-layers 12 \
             --hidden-size 768 \
             --num-attention-heads 12 \
             --seq-length 512 \
             --max-position-embeddings 512 \
-            --tokenizer-model-type bert-base-uncased"
+            --tokenizer-model-type bert-base-uncased \
+            --tokenizer-type BertWordPieceTokenizer \
+            --load-pretrained $PRETRAINED_CHECKPOINT"
 
-COMMON_TASK_ARGS_EXT="--data-dir $DATA_PATH \
-                      --load-pretrained $PRETRAINED_CHECKPOINT \
-                      --checkpoint-activations \
-                      --save-interval 10000 \
-                      --save $CHECKPOINT_PATH \
-                      --log-interval 10 \
-                      --eval-interval 1000 \
-                      --eval-iters 100 \
-                      --eval-epoch 10 \
-                      --weight-decay 1.0e-1"
+TRAIN_ARGS="--epochs 60 \
+            --batch-size 8 \
+            --lr 1e-6 \
+            --lr-decay-style linear \
+            --warmup 0.06 \
+            --weight-decay 1.0e-1"
+
+COMMON_ARGS="--checkpoint-activations \
+             --save-interval 10000 \
+             --save $CHECKPOINT_PATH \
+             --log-interval 10 \
+             --eval-interval 1000 \
+             --eval-epoch 10 \
+             --eval-iters 100"
 
 mkdir logs
-#MASTER_PORT=${MASTER_PORT} python finetune_gpt2.py \
 python -m torch.distributed.launch $DISTRIBUTED_ARGS finetune_gpt2.py \
        --experiment-name ${EXPERIMENT_NAME} \
        --task ReCoRD \
        --finetune \
+       --data-dir $DATA_PATH \
        $MODEL_ARGS \
-       $COMMON_TASK_ARGS_EXT \
-       --tokenizer-type BertWordPieceTokenizer \
-       --pool-token start \
-       --epochs 50 \
-       --batch-size 8 \
-       --lr 1e-6 \
-       --lr-decay-style linear \
-       --warmup 0.06 \
-       --cloze-eval \
+       $TRAIN_ARGS \
+       $COMMON_ARGS \
        2>&1 | tee logs/log-${DATESTR}.txt
