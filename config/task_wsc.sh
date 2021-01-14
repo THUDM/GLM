fd3ce4423b6d0e67a1449386e326dddad0d79105
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-01-14 09:43:02 +0800

    Add WSC scripts

---
+++ config/task_wsc.sh
@@ -0,0 +1,17 @@
+EXPERIMENT_NAME=blank-base-WSC
+TASK_NAME=wsc
+DATA_PATH="/root/data/superglue/WSC"
+MAX_SEQ_LEN=128
+source config/task_blocklm.sh
+
+TRAIN_ARGS="--epochs 20 \
+            --batch-size 8 \
+            --lr 1e-5 \
+            --lr-decay-style linear \
+            --warmup 0.06 \
+            --weight-decay 1.0e-1"
+
+COMMON_ARGS="--save-interval 10000 \
+             --log-interval 50 \
+             --eval-interval 1000 \
+             --eval-iters 100"
