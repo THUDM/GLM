c753906bc782f9017e525e474b2bf8eee0c21d18
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-01-19 16:17:09 +0800

    Add separate superglue args

--- scripts/finetune_superglue.sh
+++ scripts/finetune_superglue.sh
@@ -1,22 +1,9 @@
 source $1
-
-CHECKPOINT_PATH="/root/data/checkpoints"
+source scripts/superglue_args.sh
 
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
 DISTRIBUTED_ARGS="--nproc_per_node 2 --nnodes 1 --node_rank 0 --master_addr localhost --master_port $MASTER_PORT"
 DATESTR=$(date +"%m-%d-%H-%M")
 
 mkdir logs
-python -m torch.distributed.launch $DISTRIBUTED_ARGS finetune_gpt2.py \
-       --finetune \
-       --experiment-name ${EXPERIMENT_NAME} \
-       --task ${TASK_NAME} \
-       --data-dir ${DATA_PATH} \
-       --save ${CHECKPOINT_PATH} \
-       --checkpoint-activations \
-       --seq-length ${MAX_SEQ_LEN} \
-       --eval-batch-size 16 \
-       $MODEL_ARGS \
-       $TRAIN_ARGS \
-       $COMMON_ARGS \
-       2>&1 | tee logs/log-${DATESTR}.txt
+python -m torch.distributed.launch $DISTRIBUTED_ARGS finetune_gpt2.py ${finetune_options} 2>&1 | tee logs/log-${DATESTR}.txt
