394bc976df441d383946f6458b4e2658787adfa7
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-01-25 17:32:31 +0800

    Add scripts for zero-shot LM

--- scripts/evaluate_lambda.sh
+++ scripts/evaluate_lambda.sh
@@ -4,20 +4,8 @@ MASTER_PORT=$(shuf -n 1 -i 10000-65535)
 DISTRIBUTED_ARGS="--nproc_per_node 1 --nnodes 1 --node_rank 0 --master_addr localhost --master_port $MASTER_PORT"
 DATESTR=$(date +"%m-%d-%H-%M")
 
-MAX_SEQ_LEN=512
 source config/model_gpt_large.sh
-
-#EXPERIMENT_NAME=${MODEL_TYPE}-lambda_uni
-#TASK_NAME=lambda
-#DATA_PATH=/root/data/lambada_test.jsonl
-
-#EXPERIMENT_NAME=${MODEL_TYPE}-wikitext
-#TASK_NAME=wikitext
-#DATA_PATH=/root/data/wikitext-103/wiki.test.tokens
-
-EXPERIMENT_NAME=${MODEL_TYPE}-lm_uni
-TASK_NAME=language_model
-DATA_PATH=/root/data/bert-large-test.txt
+source $1
 
 python -m torch.distributed.launch $DISTRIBUTED_ARGS finetune_gpt2.py \
        --finetune \
@@ -26,9 +14,6 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS finetune_gpt2.py \
        --valid-data ${DATA_PATH} \
        --save ${CHECKPOINT_PATH} \
        --checkpoint-activations \
-       --seq-length ${MAX_SEQ_LEN} \
-       --eval-batch-size 16 \
-       --overlapping-eval 256 \
-       --unidirectional \
        $MODEL_ARGS \
+       $EVALUATE_ARGS \
        2>&1 | tee logs/log-${DATESTR}.txt
