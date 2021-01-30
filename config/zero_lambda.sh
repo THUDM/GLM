7a632ce73f201d41f4b9ba5828c48e94b8195a4f
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-01-30 21:52:44 +0800

    Add unidirectional config

--- config/zero_lambda.sh
+++ config/zero_lambda.sh
@@ -1,6 +1,5 @@
-EXPERIMENT_NAME=${MODEL_TYPE}-lambda_uni
+EXPERIMENT_NAME=${MODEL_TYPE}-lambda
 TASK_NAME=lambda
 DATA_PATH=/root/data/lambada_test.jsonl
 EVALUATE_ARGS="--eval-batch-size 16 \
-               --seq-length 512 \
-               --unidirectional"
+               --seq-length 512"
