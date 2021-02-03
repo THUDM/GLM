958ee367395429d66f2f34d4fa9d9b675a228e13
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-02-03 21:35:13 +0800

    Add scripts and config for seq2seq

--- scripts/finetune_seq2seq.sh
+++ scripts/finetune_seq2seq.sh
@@ -1,22 +1,7 @@
 source config/model_blocklm_large_generation.sh
-EXPERIMENT_NAME=${MODEL_TYPE}-cnndm
+source $1
 CHECKPOINT_PATH="/root/data/checkpoints"
 
-TASK_NAME=cnn_dm
-DATA_PATH="/root/data/cnn_dm"
-
-TRAIN_ARGS="--epochs 15 \
-            --batch-size 8 \
-            --lr 3e-5 \
-            --lr-decay-style linear \
-            --warmup 0.06 \
-            --weight-decay 1.0e-1
-            --label-smoothing 0.1"
-
-COMMON_ARGS="--save-interval 10000 \
-             --log-interval 50 \
-             --eval-interval 1000 \
-             --eval-iters 100"
 
 export NCCL_DEBUG=info
 export NCCL_IB_DISABLE=0
@@ -37,14 +22,7 @@ deepspeed --master_port ${MASTER_PORT} --num_nodes ${NUM_WORKERS} --num_gpus ${N
        --data-dir ${DATA_PATH} \
        --save ${CHECKPOINT_PATH} \
        --checkpoint-activations \
-       --src-seq-length 608 \
-       --tgt-seq-length 160 \
-       --min-tgt-length 55 \
-       --length-penalty 0.7 \
-       --no-repeat-ngram-size 3 \
-       --num-beams 5 \
-       --select-topk \
-       --eval-batch-size 4 \
        $MODEL_ARGS \
        $TRAIN_ARGS \
-       $COMMON_ARGS
+       $COMMON_ARGS \
+       $TASK_ARGS
