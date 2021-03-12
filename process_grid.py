eb334412b0f766d9f9ea36bbbfd0c8fc0e7fbecc
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-03-12 10:20:20 +0800

    Improve process_grid.py

--- process_grid.py
+++ process_grid.py
@@ -1,31 +1,48 @@
 import sys
 import os
 import json
-from tasks.superglue.finetune import default_metrics
+import glob
+import statistics
 
-root_path = sys.argv[1]
+path_pattern = sys.argv[1]
+target_type = sys.argv[2]
 best_value, best_result, best_name = None, None, None
-for entry in os.scandir(root_path):
-    if entry.is_dir():
-        result_file = os.path.join(entry.path, "results.json")
-        config_file = os.path.join(entry.path, "config.json")
-        with open(config_file) as file:
-                config = json.load(file)
-        task = config["task"].lower()
-        metric = default_metrics[task][0][0]
-        valid_result, test_result = None, None
-        if os.path.exists(result_file):
-            with open(result_file) as file:
-                lines = file.readlines()
-            if len(lines) > 1:
-                valid_result = json.loads(lines[0].strip())
-                test_result = json.loads(lines[1].strip())
-        if test_result is not None:
-            metric_value = valid_result[metric]
-            if best_value is None or metric_value > best_value:
-                best_value = metric_value
-                best_result = valid_result
-                best_name = entry.name
+mean_result = {}
+print(path_pattern)
+for entry in glob.glob(path_pattern, recursive=True):
+    valid_result, test_found = None, False
+    with open(entry) as file:
+        for line in file:
+            data = json.loads(line.strip())
+            if data["type"] == target_type:
+                valid_result = data
+            if "epoch" not in data:
+                test_found = True
+    if not test_found:
+        print(f"{entry} not completed yet")
+    if sys.argv[3] == "max":
+        metric = sys.argv[4]
+        metric_value = valid_result[metric]
+        if best_value is None or metric_value > best_value:
+            best_value = metric_value
+            best_result = valid_result
+            best_name = entry
+    elif sys.argv[3] == "mean" or sys.argv[3] == "median":
+        if mean_result:
+            for metric, value in valid_result.items():
+                if metric not in ["type", "epoch"]:
+                    mean_result[metric].append(value)
         else:
-            print(f"{entry.name} not completed yet")
-print(f"Best result found at {best_name}: {best_result}")
+            mean_result = {metric: [value] for metric, value in valid_result.items() if
+                           metric not in ["type", "epoch"]}
+
+if sys.argv[3] == "max":
+    print(f"Best result found at {best_name}: {best_result}")
+    with open(best_name) as file:
+        print(file.read())
+elif sys.argv[3] == "mean":
+    mean_result = {metric: sum(value) / len(value) for metric, value in mean_result.items()}
+    print(f"Mean result {mean_result}")
+elif sys.argv[3] == "median":
+    mean_result = {metric: statistics.median(value) for metric, value in mean_result.items()}
+    print(f"Mean result {mean_result}")
