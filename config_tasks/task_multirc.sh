503eb7d957437bd73dbce7e63567992e847389d3
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-03-13 15:15:20 +0800

    Change MultiRC config

--- config_tasks/task_multirc.sh
+++ config_tasks/task_multirc.sh
@@ -1,7 +1,7 @@
 EXPERIMENT_NAME=${MODEL_TYPE}-MultiRC
 TASK_NAME=multirc
 DATA_PATH="${DATA_ROOT}/MultiRC"
-MAX_SEQ_LEN=512
+MAX_SEQ_LEN=448
 
 LR_RANGE=(1e-5)
 EPOCH_RANGE=(10)
@@ -15,6 +15,6 @@ TRAIN_ARGS="--batch-size 16 \
             --weight-decay 1.0e-1"
 
 COMMON_ARGS="--save-interval 10000 \
-             --log-interval 50 \
+             --log-interval 5 \
              --eval-interval 1000 \
              --eval-iters 100"
