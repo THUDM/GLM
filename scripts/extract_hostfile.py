2c8258760acb19ef426c4352e6094669d6cd162f
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-03-26 10:38:38 +0800

    Change for A100 test

--- scripts/extract_hostfile.py
+++ scripts/extract_hostfile.py
@@ -5,10 +5,17 @@ import torch
 if os.path.exists("/home/hostfile.json"):
     with open("/home/hostfile.json") as file:
         hosts = json.load(file)
+    master_hosts, slave_hosts = [], []
+    for host_info in hosts:
+        if host_info["role"] == "master":
+            master_hosts.append(host_info["ip"])
+        else:
+            slave_hosts.append(host_info["ip"])
     with open("/workspace/hostfile", "w") as output:
-        for host in hosts:
-            if host["role"] == "master":
-                output.write(f"root@{host['ip']} slots=8\n")
+        for host in master_hosts:
+            output.write(f"root@{host} slots=8\n")
+        for host in slave_hosts:
+            output.write(f"root@{host} slots=8\n")
 else:
     gpu_count = torch.cuda.device_count()
     with open("/workspace/hostfile", "w") as output:
