051966b0e2956b73c825fa3b0da6b046c832f526
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-03-30 20:51:57 +0800

    Update config for blocklm

--- config/ds_block_10B.sh
+++ config/ds_block_10B.sh
@@ -16,7 +16,7 @@ gpt_options=" \
        --model-parallel-size ${MP_SIZE} \
        --num-layers 48 \
        --hidden-size 4096 \
-       --num-attention-heads 128 \
+       --num-attention-heads 64 \
        --seq-length 512 \
        --max-position-embeddings 1024 \
        --save /dataset/fd5061f6/english_data/checkpoints \
