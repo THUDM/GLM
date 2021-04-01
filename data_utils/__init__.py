78558564ee4cc825079c1529e688843448156e83
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-04-01 14:09:35 +0800

    Add log for loader scatter

--- data_utils/__init__.py
+++ data_utils/__init__.py
@@ -51,88 +51,103 @@ def get_ext(path):
 
 def get_dataset(name, tokenizer, pre_tokenize, data_parallel_rank, loader_scatter=None, no_lazy_loader=False):
     """gets dataset object based on keyword args and file at `path`"""
-    if supported_corpus(name):
-        dataset = corpora.NAMED_CORPORA[name]
-        path = dataset.PATH
-        if issubclass(dataset, corpora.PromptReader):
-            if not (exists_lazy(path, data_type='prompt') and exists_lazy(path, data_type='text')):
-                # create cached version of dataset for lazy loading if it doesn't exist
-                if torch.distributed.get_rank() == 0:
-                    print(f"Creating lazy loader for dataset {name}")
-                    prompt_writer = LazyWriter(path, data_type='prompt', is_array=pre_tokenize)
-                    text_writer = LazyWriter(path, data_type='text', is_array=pre_tokenize)
-                    writers = {'prompt': prompt_writer, 'text': text_writer}
-                    dataset(writers=writers, tokenizer=tokenizer, tokenize=pre_tokenize)
-                    prompt_writer.close()
-                    text_writer.close()
-                else:
-                    while not os.path.exists(LazyWriter.get_len_path(path, data_type='prompt')):
-                        time.sleep(1)
-            map_fn = (lambda x: x.tolist()) if pre_tokenize else None
-            if loader_scatter is not None:
-                if not (exists_scatter(path, data_type='prompt') and exists_lazy(path, data_type='text')):
-                    if torch.distributed.get_rank() == 0:
-                        print(f"Creating scatter loader for dataset {name}")
-                        prompts = LazyLoader(path, data_type='prompt', map_fn=map_fn, mem_map=True,
-                                             is_array=pre_tokenize)
-                        texts = LazyLoader(path, data_type='text', map_fn=map_fn, mem_map=True,
-                                           is_array=pre_tokenize)
-                        indices = list(range(len(texts)))
-                        random.shuffle(indices)
-                        segment_length = (len(indices) - 1) // loader_scatter + 1
-                        for i in range(loader_scatter):
-                            scatter_path = get_scatter_path(path, scatter_rank=i)
-                            prompt_writer = LazyWriter(scatter_path, data_type='prompt', is_array=pre_tokenize)
-                            text_writer = LazyWriter(scatter_path, data_type='text', is_array=pre_tokenize)
-                            for idx in indices[i * segment_length: (i + 1) * segment_length]:
-                                prompt_writer.write(prompts[idx])
-                                text_writer.write(texts[idx])
-                            prompt_writer.close()
-                            text_writer.close()
-                    else:
-                        while not (exists_scatter(path, data_type='prompt') and exists_lazy(path, data_type='text')):
-                            time.sleep(1)
-                scatter_path = get_scatter_path(path, scatter_rank=data_parallel_rank % loader_scatter)
-                prompts = LazyLoader(scatter_path, data_type='prompt', map_fn=map_fn, mem_map=True,
-                                     is_array=pre_tokenize, load_memory=no_lazy_loader)
-                texts = LazyLoader(scatter_path, data_type='text', map_fn=map_fn, mem_map=True,
-                                   is_array=pre_tokenize, load_memory=no_lazy_loader)
+    global_rank = torch.distributed.get_rank()
+    if not supported_corpus(name):
+        raise NotImplementedError('dataset %s is not supported' % name)
+    dataset = corpora.NAMED_CORPORA[name]
+    path = dataset.PATH
+    if issubclass(dataset, corpora.PromptReader):
+        if not (exists_lazy(path, data_type='prompt') and exists_lazy(path, data_type='text')):
+            # create cached version of dataset for lazy loading if it doesn't exist
+            if global_rank == 0:
+                print(f"Creating lazy loader for dataset {name}")
+                prompt_writer = LazyWriter(path, data_type='prompt', is_array=pre_tokenize)
+                text_writer = LazyWriter(path, data_type='text', is_array=pre_tokenize)
+                writers = {'prompt': prompt_writer, 'text': text_writer}
+                dataset(writers=writers, tokenizer=tokenizer, tokenize=pre_tokenize)
+                prompt_writer.close()
+                text_writer.close()
             else:
-                prompts = LazyLoader(path, data_type='prompt', map_fn=map_fn, mem_map=True,
-                                     is_array=pre_tokenize, load_memory=no_lazy_loader)
-                texts = LazyLoader(path, data_type='text', map_fn=map_fn, mem_map=True,
-                                   is_array=pre_tokenize, load_memory=no_lazy_loader)
-            text = corpora.PromptDataset(prompt_loader=prompts, text_loader=texts, tokenizer=tokenizer,
-                                         to_tokenize=not pre_tokenize)
-            if torch.distributed.get_rank() == 0:
+                while not os.path.exists(LazyWriter.get_len_path(path, data_type='prompt')):
+                    time.sleep(1)
+        map_fn = (lambda x: x.tolist()) if pre_tokenize else None
+        if loader_scatter is not None:
+            if not (exists_scatter(path, data_type='prompt', scatter_num=loader_scatter) and exists_scatter(path,
+                                                                                                            data_type='text',
+                                                                                                            scatter_num=loader_scatter)):
+                if global_rank == 0:
+                    print(f"Creating scatter loader for dataset {name}")
+                    prompts = LazyLoader(path, data_type='prompt', map_fn=map_fn, mem_map=True,
+                                         is_array=pre_tokenize)
+                    texts = LazyLoader(path, data_type='text', map_fn=map_fn, mem_map=True,
+                                       is_array=pre_tokenize)
+                    indices = list(range(len(texts)))
+                    random.shuffle(indices)
+                    segment_length = (len(indices) - 1) // loader_scatter + 1
+                    for i in range(loader_scatter):
+                        scatter_path = get_scatter_path(path, scatter_rank=i)
+                        prompt_writer = LazyWriter(scatter_path, data_type='prompt', is_array=pre_tokenize)
+                        text_writer = LazyWriter(scatter_path, data_type='text', is_array=pre_tokenize)
+                        for idx in indices[i * segment_length: (i + 1) * segment_length]:
+                            prompt_writer.write(prompts[idx])
+                            text_writer.write(texts[idx])
+                        prompt_writer.close()
+                        text_writer.close()
+                else:
+                    while not (
+                            exists_scatter(path, data_type='prompt', scatter_num=loader_scatter) and exists_scatter(
+                            path, data_type='text', scatter_num=loader_scatter)):
+                        time.sleep(1)
+            scatter_path = get_scatter_path(path, scatter_rank=data_parallel_rank % loader_scatter)
+            print(f"Rank {global_rank} is using scatter from {scatter_path}")
+            prompts = LazyLoader(scatter_path, data_type='prompt', map_fn=map_fn, mem_map=True,
+                                 is_array=pre_tokenize, load_memory=no_lazy_loader)
+            texts = LazyLoader(scatter_path, data_type='text', map_fn=map_fn, mem_map=True,
+                               is_array=pre_tokenize, load_memory=no_lazy_loader)
+        else:
+            prompts = LazyLoader(path, data_type='prompt', map_fn=map_fn, mem_map=True,
+                                 is_array=pre_tokenize, load_memory=no_lazy_loader)
+            texts = LazyLoader(path, data_type='text', map_fn=map_fn, mem_map=True,
+                               is_array=pre_tokenize, load_memory=no_lazy_loader)
+        text = corpora.PromptDataset(prompt_loader=prompts, text_loader=texts, tokenizer=tokenizer,
+                                     to_tokenize=not pre_tokenize)
+        if loader_scatter is None:
+            if global_rank == 0:
                 print(f"Create dataset {name} with {len(text)} documents")
                 for i in range(10):
                     rand_id = i if i < 5 else random.randrange(len(text))
                     sample_tokens = text[rand_id]['tokens'][:1024]
                     print(sample_tokens)
                     print(tokenizer.DecodeIds(sample_tokens).encode('utf-8'))
-            return text
-        elif issubclass(dataset, corpora.KeyReader):
-            if not (exists_lazy(path, data_type='text') and exists_lazy(path, data_type='mask')):
-                # create cached version of dataset for lazy loading if it doesn't exist
-                if torch.distributed.get_rank() == 0:
-                    text_writer = LazyWriter(path, data_type='text', is_array=pre_tokenize)
-                    mask_writer = LazyWriter(path, data_type='mask', is_array=True)
-                    writers = {'mask': mask_writer, 'text': text_writer}
-                    dataset(writers=writers, tokenizer=tokenizer, tokenize=pre_tokenize)
-                    mask_writer.close()
-                    text_writer.close()
-                else:
-                    while not os.path.exists(LazyWriter.get_len_path(path, data_type='mask')):
-                        time.sleep(1)
-            map_fn = (lambda x: x.tolist()) if pre_tokenize else None
-            masks = LazyLoader(path, data_type='mask', map_fn=map_fn, mem_map=True, is_array=True)
-            texts = LazyLoader(path, data_type='text', map_fn=map_fn, mem_map=True, is_array=pre_tokenize)
-            text = corpora.KeyDataset(mask_loader=masks, text_loader=texts, tokenizer=tokenizer,
-                                      to_tokenize=not pre_tokenize)
-            return text
-    else:
-        raise NotImplementedError('dataset %s is not supported' % name)
+        else:
+            for scatter_id in range(loader_scatter):
+                if data_parallel_rank % loader_scatter == scatter_id and data_parallel_rank // loader_scatter == 0:
+                    print(f"Create dataset {name} at scatter {scatter_id} with {len(text)} documents")
+                    for i in range(10):
+                        sample_tokens = text[i]['tokens'][:1024]
+                        print(sample_tokens)
+                        print(tokenizer.DecodeIds(sample_tokens).encode('utf-8'))
+                torch.distributed.barrier()
+        return text
+    elif issubclass(dataset, corpora.KeyReader):
+        if not (exists_lazy(path, data_type='text') and exists_lazy(path, data_type='mask')):
+            # create cached version of dataset for lazy loading if it doesn't exist
+            if global_rank == 0:
+                text_writer = LazyWriter(path, data_type='text', is_array=pre_tokenize)
+                mask_writer = LazyWriter(path, data_type='mask', is_array=True)
+                writers = {'mask': mask_writer, 'text': text_writer}
+                dataset(writers=writers, tokenizer=tokenizer, tokenize=pre_tokenize)
+                mask_writer.close()
+                text_writer.close()
+            else:
+                while not os.path.exists(LazyWriter.get_len_path(path, data_type='mask')):
+                    time.sleep(1)
+        map_fn = (lambda x: x.tolist()) if pre_tokenize else None
+        masks = LazyLoader(path, data_type='mask', map_fn=map_fn, mem_map=True, is_array=True)
+        texts = LazyLoader(path, data_type='text', map_fn=map_fn, mem_map=True, is_array=pre_tokenize)
+        text = corpora.KeyDataset(mask_loader=masks, text_loader=texts, tokenizer=tokenizer,
+                                  to_tokenize=not pre_tokenize)
+        return text
 
 
 def supported_corpus(corpus_name):
