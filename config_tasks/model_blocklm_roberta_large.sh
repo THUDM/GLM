1fc1f06904376a789e7bb130bd0003fff542d382
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-04-09 16:15:03 +0800

    Update model config

--- config_tasks/model_blocklm_roberta_large.sh
+++ config_tasks/model_blocklm_roberta_large.sh
@@ -1,4 +1,4 @@
-MODEL_TYPE="blocklm-roberta-large-250k"
+MODEL_TYPE="blocklm-roberta-large"
 MODEL_ARGS="--block-lm \
             --num-layers 24 \
             --hidden-size 1024 \
@@ -6,4 +6,4 @@ MODEL_ARGS="--block-lm \
             --max-position-embeddings 512 \
             --tokenizer-model-type roberta \
             --tokenizer-type GPT2BPETokenizer \
-            --load-pretrained ${CHECKPOINT_PATH}/blocklm-roberta-large/250000"
+            --load-pretrained ${CHECKPOINT_PATH}/blocklm-roberta-large-blank"
