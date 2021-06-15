95e8bd4f88c47e353350d8bcd9a065a16687e222
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-06-15 11:09:14 +0800

    Implement grid processing in process_grid.py

--- process_grid.py
+++ process_grid.py
@@ -9,25 +9,28 @@ target_type = sys.argv[2]
 best_value, best_result, best_name = None, None, None
 mean_result = {}
 print(path_pattern)
-for entry in glob.glob(path_pattern, recursive=True):
-    valid_result, test_found = None, False
-    with open(entry) as file:
-        for line in file:
-            data = json.loads(line.strip())
-            if data["type"] == target_type:
-                valid_result = data
-            if "epoch" not in data:
-                test_found = True
+for dir_path in glob.glob(path_pattern, recursive=True):
+    entry = os.path.basename(dir_path)
+    valid_result = None
+    test_found = os.path.exists(os.path.join(dir_path, "test_results.json"))
+    valid_path = os.path.join(dir_path, "results.json")
+    if os.path.exists(valid_path):
+        print(entry)
+        with open(valid_path) as file:
+            valid_result = json.load(file)
+    else:
+        print(f"{entry} no validation results")
+        continue
     if not test_found:
-        print(f"{entry} not completed yet")
-    if sys.argv[3] == "max":
-        metric = sys.argv[4]
+        print(f"{entry} not tested yet")
+    if target_type == "max":
+        metric = sys.argv[3]
         metric_value = valid_result[metric]
         if best_value is None or metric_value > best_value:
             best_value = metric_value
             best_result = valid_result
             best_name = entry
-    elif sys.argv[3] == "mean" or sys.argv[3] == "median":
+    elif target_type == "mean" or target_type == "median":
         if mean_result:
             for metric, value in valid_result.items():
                 if metric not in ["type", "epoch"]:
@@ -36,13 +39,11 @@ for entry in glob.glob(path_pattern, recursive=True):
             mean_result = {metric: [value] for metric, value in valid_result.items() if
                            metric not in ["type", "epoch"]}
 
-if sys.argv[3] == "max":
+if target_type == "max":
     print(f"Best result found at {best_name}: {best_result}")
-    with open(best_name) as file:
-        print(file.read())
-elif sys.argv[3] == "mean":
+elif target_type == "mean":
     mean_result = {metric: sum(value) / len(value) for metric, value in mean_result.items()}
     print(f"Mean result {mean_result}")
-elif sys.argv[3] == "median":
+elif target_type == "median":
     mean_result = {metric: statistics.median(value) for metric, value in mean_result.items()}
     print(f"Mean result {mean_result}")
