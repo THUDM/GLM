6f10e3649c03e3b54e86515dc99ecf6cef615084
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-07-01 19:26:17 +0800

    Implement extraction task

--- finetune_glm.py
+++ finetune_glm.py
@@ -456,7 +456,7 @@ if __name__ == '__main__':
         from tasks.superglue.finetune import main
     elif args.task.lower() in ['lambda', 'wikitext', 'language_model']:
         from tasks.language_model.finetune import main
-    elif args.task.lower() in ['cnn_dm', 'cnn_dm_original', 'gigaword', 'blank', 'squad_generation', 'xsum']:
+    elif args.task.lower() in ['cnn_dm', 'cnn_dm_original', 'gigaword', 'blank', 'squad_generation', 'xsum', 'extraction']:
         from tasks.seq2seq.finetune import main
     else:
         raise NotImplementedError('Task {} is not implemented.'.format(args.task))
