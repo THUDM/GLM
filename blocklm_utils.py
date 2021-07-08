f942cf0c925166e57cec2dea00f4c684af7e370a
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-07-08 19:46:02 +0800

    Add Chinese punct in blocklm_utils.py

--- blocklm_utils.py
+++ blocklm_utils.py
@@ -85,6 +85,16 @@ class ConstructBlockStrategy:
             return True
         if ':' in tok:
             return True
+        if '。' in tok:
+            return True
+        if '？' in tok:
+            return True
+        if '！' in tok:
+            return True
+        if '；' in tok:
+            return True
+        if '…' in tok:
+            return True
         if '\n' in tok:
             return True
         return False
@@ -364,7 +374,7 @@ class ConstructBlockStrategy:
                 if last_index < len(tokens):
                     sentence_spans.append((last_index, len(tokens)))
                 if not sentence_spans:
-                    print(self.tokenizer.DecodeIds(tokens[1:]).encode('utf-8'))
+                    print(self.tokenizer.DecodeIds(tokens[1:]))
                 rng.shuffle(sentence_spans)
                 block_spans, block_length = [], 0
                 for start, end in sentence_spans:
