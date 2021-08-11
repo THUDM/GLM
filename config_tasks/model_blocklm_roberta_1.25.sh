cd3ea0c0c0a9409c341576838d5ff0add39f64b2
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-08-11 19:05:00 +0800

    Update config for model_blocklm_roberta_1.25.sh

--- config_tasks/model_blocklm_roberta_1.25.sh
+++ config_tasks/model_blocklm_roberta_1.25.sh
@@ -6,6 +6,7 @@ MODEL_ARGS="--block-lm \
             --hidden-size 1152 \
             --num-attention-heads 18 \
             --max-position-embeddings 1024 \
+            --attention-scale 8.0 \
             --tokenizer-model-type roberta \
             --tokenizer-type GPT2BPETokenizer \
             --load-pretrained /dataset/c07bd62b/checkpoints/blocklm-roberta-1.25-blank04-22-14-01"
