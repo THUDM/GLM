67a428e2f70acca8a77998dbe8df62c58b86eb3d
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2021-11-11 14:46:59 +0800

    Fix prefix prompt tuning

--- model/modeling_glm.py
+++ model/modeling_glm.py
@@ -26,13 +26,53 @@ from SwissArmyTransformer.mpu.transformer import standard_attention, split_tenso
 from utils import print_rank_0
 
 
-class CachedAutoregressiveMixin(BaseMixin):
-    def __init__(self):
-        super().__init__()
+class PrefixEncoder(torch.nn.Module):
+    """
+    The torch.nn model to encode the prefix
+    Input shape: (batch-size, prefix-length)
+    Output shape: (batch-size, prefix-length, layers*hidden)
+    """
 
-    def attention_forward(self, hidden_states, mask, prefixs=None, layer_id=None, log_attention_weights=None, **kwargs):
+    def __init__(self, prefix_length, num_layers, hidden_size):
+        super().__init__()
+        self.prefix_length = prefix_length
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.embedding = torch.nn.Embedding(prefix_length, num_layers * hidden_size)
+
+    def forward(self, batch_size):
+        prefix_hidden_states = self.embedding.weight
+        prefix_hidden_states = prefix_hidden_states.unsqueeze(0).expand(batch_size, -1, -1)
+        prefix_hidden_states = prefix_hidden_states.view(
+            batch_size,
+            self.prefix_length,
+            self.num_layers,
+            self.hidden_size
+        )
+        return prefix_hidden_states
+
+
+class GLMFPrefixModel(GLMModel):
+    def __init__(self, args, transformer=None, parallel_output=True):
+        super(GLMFPrefixModel, self).__init__(args, transformer=transformer, parallel_output=parallel_output)
+        print_rank_0(f"Create prefix prompt of length {args.prefix_prompt}")
+        self.prefix_length = args.prefix_prompt
+        self.num_layers = args.num_layers
+        self.hidden_size = args.hidden_size
+        self.freeze_transformer = args.freeze_transformer
+        self.prefix_encoder = PrefixEncoder(prefix_length=args.prefix_prompt, num_layers=args.num_layers,
+                                            hidden_size=args.hidden_size)
+
+    def disable_untrainable_params(self):
+        if self.freeze_transformer:
+            print_rank_0("Freeze transformer model")
+            self.transformer.requires_grad_(False)
+            self.mixins['block_position_embedding'].requires_grad_(False)
+
+    def attention_forward(self, hidden_states, mask, layer_id=None, log_attention_weights=None, prefixs=None,
+                          **kwargs):
         attn_module = self.transformer.layers[layer_id].attention
-        prefix = prefixs[layer_id] if prefixs is not None else None
+        prefix = prefixs[:, :, layer_id] if prefixs is not None else None
         query_length = hidden_states.size(1)
         if prefix is None:  # the first time, mem is None
             mixed_raw_layer = attn_module.query_key_value(hidden_states)
@@ -63,57 +103,9 @@ class CachedAutoregressiveMixin(BaseMixin):
 
         return output, new_mem
 
-
-class PrefixEncoder(torch.nn.Module):
-    """
-    The torch.nn model to encode the prefix
-    Input shape: (batch-size, prefix-length)
-    Output shape: (batch-size, prefix-length, layers*hidden)
-    """
-
-    def __init__(self, prefix_length, num_layers, hidden_size):
-        super().__init__()
-        self.embedding = torch.nn.Embedding(prefix_length, num_layers * hidden_size)
-
-    def forward(self, prefix: torch.Tensor):
-        past_key_values = self.embedding(prefix)
-        return past_key_values
-
-
-class GLMFPrefixModel(GLMModel):
-    def __init__(self, args, transformer=None, parallel_output=True):
-        super(GLMFPrefixModel, self).__init__(args, transformer=transformer, parallel_output=parallel_output)
-        self.add_mixin('auto-regressive', CachedAutoregressiveMixin())
-        print_rank_0(f"Create prefix prompt of length {args.prefix_prompt}")
-        self.prefix_length = args.prefix_prompt
-        self.num_layers = args.num_layers
-        self.hidden_size = args.hidden_size
-        self.freeze_transformer = args.freeze_transformer
-        self.prefix_encoder = PrefixEncoder(prefix_length=args.prefix_prompt, num_layers=args.num_layers,
-                                            hidden_size=args.hidden_size)
-
-    def disable_untrainable_params(self):
-        if self.freeze_transformer:
-            print_rank_0("Freeze transformer model")
-            self.transformer.requires_grad_(False)
-
-    def get_prompt(self, batch_size):
-        prefix_hidden_states = self.prefix_encoder.embedding.weight.half()
-        prefix_hidden_states = prefix_hidden_states.unsqueeze(0).expand(batch_size, -1, -1)
-        # bsz, seqlen, _ = past_key_values.shape
-        prefix_hidden_states = prefix_hidden_states.view(
-            batch_size,
-            self.prefix_length,
-            self.num_layers,
-            self.hidden_size
-        )
-        prefix_hidden_states = prefix_hidden_states.permute([2, 0, 1, 3]).split(1)
-        prefix_hidden_states = [value[0] for value in prefix_hidden_states]
-        return prefix_hidden_states
-
     def forward(self, input_ids, position_ids, attention_mask, *, branch_input=None, **kw_args):
         batch_size, seq_length = input_ids.shape[0], input_ids.shape[1]
-        prompt_hidden_states = self.get_prompt(batch_size=batch_size)
+        prompt_hidden_states = self.prefix_encoder(batch_size=batch_size)
         attention_mask = torch.cat(
             (attention_mask.new_ones(batch_size, 1, seq_length, self.prefix_length), attention_mask), dim=-1)
         output, *mems = super().forward(input_ids, position_ids, attention_mask, prefixs=prompt_hidden_states)
