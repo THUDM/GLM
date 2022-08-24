626569d72b129b01466de6d8470c23d5bc48c67c
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2022-08-24 18:43:10 +0800

    Fix length error in BeamSearchScorer.finalize

--- generation_utils.py
+++ generation_utils.py
@@ -293,7 +293,7 @@ class BeamSearchScorer(BeamScorer):
                 best.append((best_hyp, mems, score))
 
         # prepare for adding eos
-        sent_max_len = max(sent_lengths.max().item(), self.max_length)
+        sent_max_len = sent_lengths.max().item()
         decoded: torch.LongTensor = input_ids.new(batch_size * self.num_beam_hyps_to_keep, sent_max_len)
         scores = final_beam_scores.new(batch_size * self.num_beam_hyps_to_keep)
         # shorter batches are padded if needed
