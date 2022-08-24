ff75488dda4a143cf24dfa9b466d49e52716108e
Zhengxiao Du <zx-du20@mails.tsinghua.edu.cn>
2022-08-24 09:51:11 +0800

    Fix beam search

--- generation_utils.py
+++ generation_utils.py
@@ -293,7 +293,8 @@ class BeamSearchScorer(BeamScorer):
                 best.append((best_hyp, mems, score))
 
         # prepare for adding eos
-        sent_max_len = min(sent_lengths.max().item(), self.max_length)
+        breakpoint()
+        sent_max_len = max(sent_lengths.max().item(), self.max_length)
         decoded: torch.LongTensor = input_ids.new(batch_size * self.num_beam_hyps_to_keep, sent_max_len)
         scores = final_beam_scores.new(batch_size * self.num_beam_hyps_to_keep)
         # shorter batches are padded if needed
